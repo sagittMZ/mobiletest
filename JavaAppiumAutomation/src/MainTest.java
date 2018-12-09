e3982e1276cfe2adf553da1d0d53d52ac7d69b0d
sagitt <sagitt81@gmail.com>
2018-12-09 18:40:58 +0300

    test2_getClassNumber

--- JavaAppiumAutomation/src/MainTest.java
+++ JavaAppiumAutomation/src/MainTest.java
@@ -2,21 +2,36 @@ import org.junit.Test;
 
 public class MainTest { //extends CoreTestCase{
    // MathHelper Math = new MathHelper();
-
+private int class_number;
     @Test
     public void testGetLocalNumber()
     {
-     int a = this.getLocalNumber();
-     if (a==14)
+        int a = this.getLocalNumber();
+        if (a==14)
+        {
+            System.out.println("The number is 14");
+        } else
+        {
+            System.out.println("The number is  not 14");
+        }
+    }
+    @Test
+    public void testGetClassNumber()
+    {
+        class_number = this.getClassNumber();
+     if (class_number>45)
      {
-         System.out.println("The number is 14");
+         System.out.println("The class_number is more than 45");
      } else
      {
-         System.out.println("The number is  not 14");
+         System.out.println("The class_number is less or equal than 45");
      }
     }
 
     public int getLocalNumber() {
         return 14;
+    }
+    public int getClassNumber() {
+        return 20;
     }
 }
